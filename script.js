3d743c31ef2648041cfb5c9cc3405582cc1ffffa
taher2552 <stardom.taher@gmail.com>
2022-12-03 09:14:33 +0530

    changes

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const qualificationOfPerson = document.getElementById("qualification");
 const panNumberOfPerson = document.getElementById("pan");
 const errorMessage = document.querySelector(".error_msg");
 const tableDisplayInfo=document.getElementById('table_body');
+const searchPanNumber=document.getElementById('search');
 
 let count=0;
+let nameEdit, ageEdit, qualificationEdit, panNumberEdit;
+let panArray=[];
+
 
 let personDetailsArray=JSON.parse(localStorage.getItem("person"));
 if(!personDetailsArray){
@@ -33,13 +37,15 @@ function resetForm() {
 
 function displayPersonInformation(personDetailsObject){
    
-    tableDisplayInfo.innerHTML+=`<tr id=${personDetailsObject.id}><td>${personDetailsObject.name}</td><td>${personDetailsObject.age}</td><td>${personDetailsObject.qualification}</td><td>${personDetailsObject.panNumber}</td><td><button class="delete btn">Delete</button><button class="edit btn">Edit</button></td></tr>`
+    tableDisplayInfo.innerHTML+=`<tr id=${personDetailsObject.id}><td>${personDetailsObject.name}</td><td>${personDetailsObject.age}</td><td>${personDetailsObject.qualification}</td><td class="pan">${personDetailsObject.panNumber}</td><td><button class="delete btn">Delete</button><button class="edit btn">Edit</button></td></tr>`
 
 
 }
 
 function addDetails(e) {
   e.preventDefault();
+
+  if(addButton.innerText=="Add"){
   
   if (nameOfPerson.value == "") {
     errorMessage.innerText = "*Please Enter your name";
@@ -108,12 +114,34 @@ function addDetails(e) {
   localStorage.setItem('person', JSON.stringify(personDetailsArray));
 
   displayPersonInformation(personDetailsObject);
+  panArray.push(panNumberOfPerson.value);
+  console.log(panArray)
 
   resetForm();
+ 
+}
+else if(addButton.innerText=="Save"){
+
+  saveEditedPersonDetails(e, nameOfPerson.value, ageOfPerson.value, qualificationOfPerson.value, panNumberOfPerson.value);
+
+}
 }
 
 function deletePersonDetails(e){
-    console.log(e)
+ 
+  personDetailsArray.map((val,index)=>{
+      if(val.id===parseInt(e.path[2].id)){
+        let confirm=window.confirm("Are you sure you want to delete this??")
+        if(confirm){
+          personDetailsArray.splice(index,1); 
+          e.path[2].remove();  
+        }
+      }
+      
+  })
+  
+  
+  localStorage.setItem("person", JSON.stringify(personDetailsArray));
 }
 
 function sortingByName(e){
@@ -135,7 +163,7 @@ function sortingByName(e){
     displayPersonInformation(obj);
   })
 }
-  if(e.target.innerHTML=="a-z"){
+  if(e.target.innerHTML=="Z-A"){
   function descending( a, b ) {
     if ( a.name < b.name ){
       return 1;
@@ -156,6 +184,69 @@ function sortingByName(e){
     
   }
 
+  function editPersonDetails(e){
+     nameEdit=e.path[2].firstElementChild;
+      ageEdit=nameEdit.nextElementSibling;
+      qualificationEdit=ageEdit.nextElementSibling;
+      panNumberEdit=qualificationEdit.nextElementSibling;
+    // console.log(panNumberEdit);
+
+    nameOfPerson.value=nameEdit.innerText;
+    ageOfPerson.value=ageEdit.innerText;
+    qualificationOfPerson.value=qualificationEdit.innerText;
+    panNumberOfPerson.value=panNumberEdit.innerText;
+
+     addButton.innerText="Save";
+
+   
+    
+}
+
+function saveEditedPersonDetails(e,name,age,qualification,panNumber){
+  e.preventDefault();
+  // console.log(formInput.value,taskToEdit)
+
+ 
+  personDetailsArray.map((val)=>{
+
+       //console.log(val.name, nameEdit.text)
+
+      if(val.name==nameEdit.textContent){
+      // val.task=formInput.value;
+        //  console.log("hellllllo")
+       val.name=nameOfPerson.value;
+       
+      }
+      if(val.age==ageEdit.textContent){
+      
+       val.age=ageOfPerson.value;
+      }
+      if(val.qualification==qualificationEdit.textContent){
+      // val.task=formInput.value;
+       val.qualification=qualificationOfPerson.value;
+      }
+      if(val.panNumber==panNumberEdit.textContent){
+      // val.task=formInput.value;
+       val.panNumber=panNumberOfPerson.value;
+      }
+    
+       })
+
+  nameEdit.innerText=name;
+  ageEdit.innerText=age;
+  qualificationEdit.innerText=qualification;
+  panNumberEdit.innerText=panNumber;
+  localStorage.setItem("person",JSON.stringify(personDetailsArray));
+ 
+
+  addButton.innerText='Add';
+}
+
+
+
+
+
+
 
 
 
@@ -164,7 +255,9 @@ addButton.addEventListener("click", addDetails);
 tableDisplayInfo.addEventListener("click",(e)=>{
     if(e.target.classList.contains('delete')){
         deletePersonDetails(e);
-    
+    }
+    else if(e.target.classList.contains('edit')){
+      editPersonDetails(e);
     }
 })
 
@@ -173,3 +266,35 @@ formDetails.addEventListener('click', (e)=>{
     sortingByName(e);
   }
 })
+
+searchPanNumber.addEventListener('keyup',(e)=>{
+e.preventDefault();
+tableDisplayInfo.innerHTML="";
+
+const input=searchPanNumber.value;
+console.log(input)
+
+let filter=personDetailsArray.filter((val)=>val.panNumber.includes(input));
+console.log(filter)
+
+filter.map((val)=>{
+  displayPersonInformation(val);
+})
+
+
+
+
+})
+
+$('input[type=search]').on('search', function () {
+  // search logic here
+  // this function will be executed on click of X (clear button)
+  tableDisplayInfo.innerHTML="";
+  personDetailsArray.map((obj)=>{
+    displayPersonInformation(obj);
+  })
+});
+
+
+
+
